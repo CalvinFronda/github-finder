1e06f56f00ae8562d0fd14aa3931d2e706c0ff58
Calvin <cfronda209@gmail.com>
2019-08-28 17:19:46 -0700

    final

---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>About this App</h1>
+      <p>App to seach Github users</p>
+      <p>Github V.01</p>
+    </div>
+  );
+};
+
+export default About;
